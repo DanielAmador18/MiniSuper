666aeb193bce1b5e2336df86fda7c8e3174b334c
DanielDev <danielmedina051216@gmail.com>
2025-06-28 11:13:39 -0700

    Agregar .gitignore, limpiar archivos innecesarios y estructura basica del programa

---
+++ GestorMiniSuper/GestorMiniSuper/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Supersito
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            Ventas ventas = new Ventas();
+        }
+
+        public void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1.Registrar venta");
+
+                Console.WriteLine("2.Listar productos");
+
+                Console.WriteLine("3.Agregar producto");
+
+                Console.WriteLine("0.Salir");
+
+                Console.WriteLine("Elige una opcion:");
+                int opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+
+                    case 1: ventas.RegistrarVenta();break;
+
+
+                }
+            }
+        }
+    }
+
+    class Inventario
+    {
+
+    }
+
+     class Ventas
+    {
+
+        public void RegistrarVenta()
+        {
+            
+        }
+
+    }
+}
